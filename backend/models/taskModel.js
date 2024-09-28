ea81e9b504a98e5e35d568d27c30f4fc2cf52e4a
belizario87 <leonardobelizario88@gmail.com>
2024-09-28 16:38:14 -0300

    feat(taskModel): Add metodo para adicionar tarefa
    Foi feita uma alteração no metodo de selecionar todas as tarefas quando trocamos a conexão

--- backend/models/taskModel.js
+++ backend/models/taskModel.js
@@ -1,9 +1,26 @@
-const connection = require("./connection");
-const getAll = async function () {
-  const tasks = await connection.execute("SELECT * FROM tasks ");
-  return tasks;
+const getConn = require("./connection");
+
+const getAll = async () => {
+  const conn = await getConn();
+  const [rows] = await conn.query("SELECT * FROM tasks");
+  conn.release();
+  console.log(rows);
+
+  return rows;
+};
+
+const createTask = async (task) => {
+  const conn = await getConn();
+  const { title } = task;
+  const dateUtc = await new Date(Date.now()).toUTCString();
+  const query = "INSERT INTO tasks(title, status, created_at) VALUES (?, ?, ?)";
+
+  const [createdTask] = await conn.query(query, [title, "pendente", dateUtc]);
+
+  return createdTask;
 };
 
 module.exports = {
   getAll,
+  createTask,
 };
